89852f1edd0cc3d0274a47afb608de16b7805590
Maxim Evlampiev <max1361@yandex.ru>
2023-02-22 15:21:54 +0300

    Added find max

---
+++ Example006_Base/Program.cs
@@ -0,0 +1,34 @@
+﻿int a = 2;
+int b = 6;
+int c = 8;
+int d = 4;
+int e = 1;
+
+int max = a;
+
+if (a > max)
+{
+    max = a;
+}
+
+if (b > max)
+{
+    max = b;
+}
+
+if (c > max)
+{
+    max = c;
+}
+
+if (d > max)
+{
+    max = d;
+}
+
+if (e > max)
+{
+    max = e;
+}
+
+Console.WriteLine("Max = " + max);
