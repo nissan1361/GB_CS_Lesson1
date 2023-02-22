72d31e2e4f918f0d7953ec117a52603658186396
Maxim Evlampiev <max1361@yandex.ru>
2023-02-22 15:05:45 +0300

    Added if Masha

---
+++ Example005_if_else/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите Ваше имя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это же МАША!!!");
+}
+else
+{
+    Console.WriteLine("Привет, " + username + "!");
+}
